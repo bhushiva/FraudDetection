12752a639dadb28ad0f45215c226dda648d65740
bhushiva <b.shivagunde@gmail.com>
2023-07-30 13:42:31 +0530

    Commit the sum function

--- data_Processing.py
+++ data_Processing.py
@@ -2,13 +2,3 @@
 print("Add all the data processing code related to fraud detection")
 
 print("Also committing it without -m and massage")
-
-
-a = 5
-b= 7
-
-def sum(a,b):
-    c= a+b
-    return c
-
-print("Added new function sum which is not working")
